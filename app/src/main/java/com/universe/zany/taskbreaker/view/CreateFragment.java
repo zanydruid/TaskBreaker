6f54030d4f92c0e56f7dfd3a5b0ed1a71faba819
zanydruid <zoom198852800>
2017-10-01 23:36:29 -0400

    rebuild task create, single create tested.

--- app/src/main/java/com/universe/zany/taskbreaker/view/CreateFragment.java
+++ app/src/main/java/com/universe/zany/taskbreaker/view/CreateFragment.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -26,17 +27,20 @@ import com.universe.zany.taskbreaker.core.Task;
 import com.universe.zany.taskbreaker.injection.MainApplication;
 import com.universe.zany.taskbreaker.util.DatePickerFragment;
 import com.universe.zany.taskbreaker.viewmodels.CreateViewModel;
-import com.universe.zany.taskbreaker.viewmodels.TaskViewModel;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.inject.Inject;
 
 public class CreateFragment extends Fragment implements DatePickerDialog.OnDateSetListener, DialogInterface.OnClickListener{
+
+    //TODO
+    // redesign create date and deadline
 
     private static final String YEAR = "year";
     private static final String MONTH = "month";
@@ -46,7 +50,7 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
     ViewModelProvider.Factory factory;
     private CreateViewModel viewModel;
 
-    private boolean useStartDatePicker;
+    //private boolean useStartDatePicker;
     private Task mTask;
     private int mYear;
     private int mMonth;
@@ -54,16 +58,13 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
     private List<String> emptySlots;
 
     // views
-    TextView startDateTextView;
     TextView deadlineTextView;
-    ImageButton startDatePicker;
     ImageButton deadlinePicker;
     Spinner typeSpinner;
     TextView durationTextView;
     EditText durationInputEdit;
     Spinner durationUnitSpinner;
     EditText contentEdit;
-    Button cancelButton;
     Button createButton;
     // dialog fragment
     DatePickerFragment datePickerFragment;
@@ -90,14 +91,17 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
                 .getTaskComponent()
                 .inject(this);
 
-        mYear = getArguments().getInt(YEAR);
-        mMonth = getArguments().getInt(MONTH);
-        mDay = getArguments().getInt(DAY);
-        Calendar cal = createCal(mYear, mMonth, mDay);
-        mTask = new Task();
-        mTask.setCreated(cal.getTime());
-        datePickerFragment = DatePickerFragment.newDialog(this);
+        Bundle bundle = getArguments();
+        mYear = bundle.getInt(YEAR, 2017);
+        mMonth = bundle.getInt(MONTH, 11);
+        mDay = bundle.getInt(DAY, 31);
 
+        // init task object
+        mTask = new Task();
+        mTask.setCreated(new Date());
+        mTask.setDeadline(new Date());
+
+        datePickerFragment = DatePickerFragment.newDialog(this);
     }
 
     @Override
@@ -112,29 +116,18 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         // Inflate the layout for this fragment
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_create, container, false);
 
-        // set up view
-        startDateTextView = view.findViewById(R.id.frg_create_startdatepick_textview);
+        // set up views
+        // deadline text view
         Calendar cal = createCal(mYear, mMonth, mDay);
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
         String date = format.format(cal.getTime());
-        startDateTextView.setText(date);
-
         deadlineTextView = view.findViewById(R.id.frg_create_deadlinepick_textview);
-        startDatePicker = view.findViewById(R.id.frg_create_startdate_pick_button);
-        startDatePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // set useStartDatePicker to true
-                useStartDatePicker = true;
-                datePickerFragment.show(getFragmentManager(), "date_picker");
-            }
-        });
+        deadlineTextView.setText(date);
         deadlinePicker = view.findViewById(R.id.frg_create_deadline_pick_button);
         deadlinePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // set useStartDatePicker to false
-                useStartDatePicker = false;
                 datePickerFragment.show(getFragmentManager(), "date_picker");
             }
         });
@@ -187,14 +180,6 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         durationUnitSpinner.setVisibility(View.INVISIBLE);
 
         contentEdit = view.findViewById(R.id.frg_create_content_edit);
-        cancelButton = view.findViewById(R.id.frg_create_cancel_button);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO cancel creation of task, return to previous fragment
-
-            }
-        });
 
         createButton = view.findViewById(R.id.frg_create_ok_button);
         createButton.setOnClickListener(new View.OnClickListener() {
@@ -206,19 +191,20 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
                     for (String slot : emptySlots) {
                         sb.append(slot + "\n");
                     }
-                    Toast.makeText(getContext(), "Please fill in :\n" + sb.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "Error(s) :\n" + sb.toString(), Toast.LENGTH_LONG).show();
                     return;
                 }
-                // 2. build dialog title $ message and show dialog
+                // 2. build dialog title & message and show dialog
                 String message = createConfirmMessage();
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle(R.string.confirm_create_title)
                         .setMessage(message);
+                builder.setCancelable(false);
                 builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         conductCreate();
-                        //dialogInterface.cancel();
+                        goBack();
                     }
                 });
 
@@ -238,10 +224,8 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
                     }
                 });
                 builder.create().show();
-
             }
         });
-
         return view;
     }
 
@@ -254,13 +238,10 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         Calendar cal = createCal(i, i1, i2);
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
         String date = format.format(cal.getTime());
-        if (useStartDatePicker) {
-            startDateTextView.setText(date);
-            mTask.setCreated(cal.getTime());
-        } else {
-            deadlineTextView.setText(date);
-            mTask.setDeadline(cal.getTime());
-        }
+
+        deadlineTextView.setText(date);
+        mTask.setDeadline(cal.getTime());
+
     }
 
     @Override
@@ -281,28 +262,32 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         boolean passCheck = true;
         emptySlots = new ArrayList<>();
 
-        if (mTask.getCreated() == null) {
-            emptySlots.add("Start Date");
-            passCheck = false;
-        }
-
         if (mTask.getDeadline() == null) {
-            emptySlots.add("Deadline");
+            emptySlots.add("Please fill in Deadline");
             passCheck = false;
         }
 
         if (mTask.getType() == -1) {
-            emptySlots.add("Task Type");
+            emptySlots.add("Please choose a Task Type");
             passCheck = false;
         }
 
         if (mTask.getType() != 0 && durationInputEdit.getText().toString().length() == 0) {
-            emptySlots.add("Task Duration");
+            emptySlots.add("Please fill in Task Duration");
             passCheck = false;
         }
 
         if (contentEdit.getText().toString().length() == 0) {
-            emptySlots.add("Task Content");
+            emptySlots.add("Please fill in mTask Content");
+            passCheck = false;
+        }
+
+        Calendar today = new GregorianCalendar();
+        today.set(Calendar.YEAR, mYear);
+        today.set(Calendar.MONTH, mMonth);
+        today.set(Calendar.DAY_OF_MONTH, mDay - 1);
+        if (mTask.getDeadline().getTime() < today.getTimeInMillis()) {
+            emptySlots.add("You can't create task in the past!!!");
             passCheck = false;
         }
 
@@ -349,7 +334,6 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
                     break;
             }
         }
-        Toast.makeText(getContext(), "A new task created!!!", Toast.LENGTH_LONG);
     }
 
     private String createConfirmMessage() {
@@ -359,7 +343,6 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         if (content.length() > 30) {
             content = content.substring(0, 30) + "...";
         }
-        String startDate = startDateTextView.getText().toString();
         String deadline = deadlineTextView.getText().toString();
         String type = typeSpinner.getSelectedItem().toString();
         int typePosition = typeSpinner.getSelectedItemPosition();
@@ -367,7 +350,6 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
         String unit = durationUnitSpinner.getSelectedItem().toString();
         // build message
         messageBuilder.append(content + "\n");
-        messageBuilder.append("Start Date: " + startDate + "\n");
         messageBuilder.append("Deadline: " + deadline + "\n");
 
         if (typePosition != 0) {
@@ -377,4 +359,10 @@ public class CreateFragment extends Fragment implements DatePickerDialog.OnDateS
 
         return messageBuilder.toString();
     }
+
+    private void goBack() {
+        getActivity().onBackPressed();
+    }
+
+
 }
