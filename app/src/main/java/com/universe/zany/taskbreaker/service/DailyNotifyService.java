3502a1f9f3c1204daa35c1992ca11e3626ce3614
zanydruid <zoom198852800>
2017-10-10 11:59:17 -0400

    configure setting screen and notification setup.

--- app/src/main/java/com/universe/zany/taskbreaker/service/DailyNotifyService.java
+++ app/src/main/java/com/universe/zany/taskbreaker/service/DailyNotifyService.java
@@ -5,29 +5,75 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.universe.zany.taskbreaker.data.repository.TaskRepository;
+import com.universe.zany.taskbreaker.injection.MainApplication;
+import com.universe.zany.taskbreaker.view.CreateActivity;
 import com.universe.zany.taskbreaker.view.DayActivity;
+
+import java.util.Calendar;
+
+import javax.inject.Inject;
+
+import static com.universe.zany.taskbreaker.util.SyncLiveData.getValue;
 
 public class DailyNotifyService extends IntentService{
 
     private static final String TAG = "DailyNotifyService";
+    public static final int DAILY_NOTIFICATION_ID = 10000;
+
+    @Inject
+    TaskRepository repo;
 
     public DailyNotifyService() {
         super("Daily notification service");
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        ((MainApplication)getApplication())
+                .getTaskComponent()
+                .inject(this);
+    }
+
+    @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Log.i(TAG, "Setting notification...");
-        Intent notifyIntent = new Intent(getApplicationContext(), DayActivity.class);
+        Calendar cal = Calendar.getInstance();
+        Intent notifyIntent;
+        Bundle bundle = new Bundle();
+        bundle.putInt("today_year", cal.get(Calendar.YEAR));
+        bundle.putInt("today_month", cal.get(Calendar.MONTH));
+        bundle.putInt("today_day", cal.get(Calendar.DAY_OF_MONTH));
+
+        int numOfTasks = 0;
+        try {
+            numOfTasks = getValue(this.repo.getTasksInDay(cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH))).size();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String content;
+        if (numOfTasks == 0) {
+            content = "You don't have task today, let's create one.";
+            notifyIntent = new Intent(getApplicationContext(), CreateActivity.class);
+        } else {
+            content = "You have " + numOfTasks + " task(s) today.";
+            notifyIntent = new Intent(getApplicationContext(), DayActivity.class);
+        }
+
+        notifyIntent.putExtras(bundle);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, notifyIntent, 0);
-        int numOfTasks = intent.getExtras().getInt("daily_num_tasks");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setContentTitle("Task Breaker")
-                .setContentText("You have " + numOfTasks + " tasks today.")
+                .setContentText(content)
                 .setSmallIcon(android.R.drawable.ic_dialog_alert)
                 .setContentIntent(pendingIntent);
 
@@ -36,7 +82,7 @@ public class DailyNotifyService extends IntentService{
 
         synchronized (notificationManager) {
             notificationManager.notify();
-            notificationManager.notify(1, builder.build());
+            notificationManager.notify(DAILY_NOTIFICATION_ID, builder.build());
         }
     }
 }
--- app/src/main/java/com/universe/zany/taskbreaker/view/SettingFragment.java
+++ app/src/main/java/com/universe/zany/taskbreaker/view/SettingFragment.java
@@ -2,20 +2,24 @@ package com.universe.zany.taskbreaker.view;
 
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.universe.zany.taskbreaker.R;
 import com.universe.zany.taskbreaker.service.DailyNotifyService;
@@ -25,6 +29,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class SettingFragment extends Fragment implements TimePickerDialog.OnTimeSetListener{
+
+    private static final int DAILY_NOTIFICATION_ALARM_ID = 901;
 
     private TimePickerFragment timePicker;
     private Calendar dailyNotificationCal;
@@ -53,6 +59,21 @@ public class SettingFragment extends Fragment implements TimePickerDialog.OnTime
                              Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.fragment_setting, container, false);
+        // daily notification ===== start
+        dailyNotificationSwitch = view.findViewById(R.id.frg_setting_daily_notify_switch);
+        dailyNotificationSwitch.setChecked(getDailyNotificationState());
+        dailyNotificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    dailyNotificationTimePicker.setVisibility(View.VISIBLE);
+
+                } else {
+                    dailyNotificationTimePicker.setVisibility(View.INVISIBLE);
+                    cancelDailyNotification();
+                }
+            }
+        });
 
         dailyNotificationTimePicker = view.findViewById(R.id.frg_setting_dailynotify_timepicker_textview);
         dailyNotificationTimePicker.setOnClickListener(new View.OnClickListener() {
@@ -61,26 +82,19 @@ public class SettingFragment extends Fragment implements TimePickerDialog.OnTime
                 timePicker.show(getFragmentManager(), "time_picker");
             }
         });
-        dailyNotificationTimePicker.setVisibility(View.INVISIBLE);
-
-        dailyNotificationSwitch = view.findViewById(R.id.frg_setting_daily_notify_switch);
-        dailyNotificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b) {
-                    // TODO make time picker visible
-                    dailyNotificationTimePicker.setVisibility(View.VISIBLE);
-
-                } else {
-                    // TODO cancel notification
-                    // TODO make time picker invisible
-                    dailyNotificationTimePicker.setVisibility(View.INVISIBLE);
-
-                }
-            }
-        });
+        dailyNotificationTimePicker.setText(getDailyNotificationTime());
+        if (!getDailyNotificationState()) {
+            dailyNotificationTimePicker.setVisibility(View.INVISIBLE);
+        }
+        // daily notification ===== end
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        dailyNotificationSwitch.setChecked(getDailyNotificationState());
     }
 
     @Override
@@ -90,23 +104,62 @@ public class SettingFragment extends Fragment implements TimePickerDialog.OnTime
         SimpleDateFormat format = new SimpleDateFormat("HH:mm");
         String time = format.format(dailyNotificationCal.getTime());
         dailyNotificationTimePicker.setText(time);
-        //setDailyNotification();
+        saveDailyNotificationTime(time);
+        setDailyNotification();
     }
 
-    //==================Daily notification==================
+    //==================Daily notification================== start
 
     public void setDailyNotification() {
         mAlarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
         Intent dailyNotifyIntent = new Intent(getContext(), DailyNotifyService.class);
-        PendingIntent dailyNotifyPendingIntent = PendingIntent.getService(getContext(), 1,
+        PendingIntent dailyNotifyPendingIntent = PendingIntent.getService(getContext(), DAILY_NOTIFICATION_ALARM_ID,
                 dailyNotifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // set alarm
         mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, dailyNotificationCal.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, dailyNotifyPendingIntent);
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES, dailyNotifyPendingIntent);
+        saveDailyNotificationState(true);
+        Toast.makeText(getContext(), "Daily notification on.", Toast.LENGTH_SHORT).show();
     }
 
     public void cancelDailyNotification() {
+        NotificationManager notiManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notiManager.cancel(DailyNotifyService.DAILY_NOTIFICATION_ID);
 
+        mAlarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+        Intent dailyNotifyIntent = new Intent(getContext(), DailyNotifyService.class);
+        PendingIntent dailyNotifyPendingIntent = PendingIntent.getService(getContext(), DAILY_NOTIFICATION_ALARM_ID,
+                dailyNotifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mAlarmManager.cancel(dailyNotifyPendingIntent);
+        dailyNotifyPendingIntent.cancel();
+        saveDailyNotificationState(false);
+        Toast.makeText(getContext(), "Daily notification off.", Toast.LENGTH_SHORT).show();
     }
+
+    public void saveDailyNotificationState(boolean state) {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.daily_notification_state), state);
+        editor.commit();
+    }
+
+    public boolean getDailyNotificationState() {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getBoolean(getString(R.string.daily_notification_state), false);
+    }
+
+    public void saveDailyNotificationTime(String time) {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.daily_notification_time), time);
+        editor.commit();
+    }
+
+    public String getDailyNotificationTime() {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getString(getString(R.string.daily_notification_time), "Pick a time");
+    }
+
+    //==================Daily notification================== end
 }
